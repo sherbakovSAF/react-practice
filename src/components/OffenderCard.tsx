14588d5cf9c9c3b29f8bb66c540eb17a018149d0
SherbakovSAF <sherbakovsaf@gmail.com>
2025-05-16 19:25:53 +0300

    feat: создал карточку преступника

---
+++ src/components/OffenderCard.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from "react";
+import type { Offender_I } from "../types/OffenderType";
+import Button from "./Button";
+import SearchLevel from "./SearchLevel";
+import styles from "./OffenderCard.module.scss";
+import CashIcon from "../assets/cash-icon.svg";
+
+interface OffenderCardProps {
+  offender: Offender_I;
+  onBusted: () => void;
+}
+
+const OffenderCard: React.FC<OffenderCardProps> = ({ offender, onBusted }) => {
+  const getFormatFee = useMemo(
+    () =>
+      new Intl.NumberFormat("ru-Ru", {
+        style: "currency",
+        currency: "USD",
+      }).format(offender.fee),
+    [offender.fee]
+  );
+
+  console.log(CashIcon);
+  return (
+    <article className={styles.offender}>
+      <div className={styles.offender_img}>
+        <img src={offender.avatar} alt="" />
+      </div>
+      <div className={styles.offender_info}>
+        <p className={styles.offender_info_name}>
+          <strong>{offender.name}</strong>
+        </p>
+        <p>Возраст {offender.age}</p>
+        <p className={styles.offender_info_city}>{offender.city}</p>
+      </div>
+      <SearchLevel lvl={offender.searchLvl} />
+      <p className={styles.offender_fee}>
+        <img src={CashIcon} alt="Иконка денег" />
+        <span> {getFormatFee}</span>
+      </p>
+
+      <hr />
+      <Button onClick={() => onBusted()}>Я поймал</Button>
+    </article>
+  );
+};
+
+export default OffenderCard;
---
+++ src/types/OffenderType.ts
@@ -0,0 +1,11 @@
+export interface Offender_I {
+  id: number;
+  name: string;
+  avatar: string;
+  city: string;
+  age: number;
+  fee: number;
+  searchLvl: number;
+  isBusted: boolean;
+  createdAt: string;
+}
