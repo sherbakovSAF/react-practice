1f5005059e449aa831cfe159ae642a0d2f9c5b5a
SherbakovSAF <sherbakovsaf@gmail.com>
2025-05-23 22:57:40 +0300

    feat: исправил callback

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,45 +1,48 @@
+import React from "react";
 import type { SelectHTMLAttributes } from "react";
 import styles from "./Select.module.scss";
 import clsx from "clsx";
+import type { OptionItem_I } from "../types/SettingsTypes";
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   view?: "primary" | "secondary";
-  options: { code: string; title: string }[];
-  onOptionSelect: (option: { code: string; title: string }) => void;
-  selected?: { code: string; title: string } | null;
+  options: OptionItem_I[];
+  onOptionSelect: (option: OptionItem_I | null) => void;
+  selected?: OptionItem_I | null;
 }
 
-const Select: React.FC<SelectProps> = ({
-  view = "primary",
-  options,
-  onOptionSelect,
-  selected,
-  ...props
-}) => {
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedCode = e.target.value;
-    const selectedOption = options.find((opt) => opt.code === selectedCode);
-    if (selectedOption) {
-      onOptionSelect(selectedOption);
-    }
-  };
+const Select: React.FC<SelectProps> = React.memo(
+  ({
+    view = "primary",
+    options,
+    onOptionSelect,
+    selected = null,
+    ...props
+  }) => {
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedOption = options.find((opt) => opt.code === e.target.value);
+      onOptionSelect(selectedOption ?? null);
+    };
 
-  return (
-    <select
-      className={clsx(
-        styles.select,
-        view === "secondary" && styles["select--secondary"]
-      )}
-      onChange={handleChange}
-      value={selected?.code}
-      {...props}
-    >
-      {options.map((option) => (
-        <option key={option.code} value={option.code}>
-          {option.title}
+    return (
+      <select
+        className={clsx(
+          styles.select,
+          view === "secondary" && styles["select--secondary"]
+        )}
+        onChange={handleChange}
+        value={selected ? selected.code : "null"}
+        {...props}
+      >
+        <option key={"null"} value={"null"}>
+          Не выбрано
         </option>
-      ))}
-    </select>
-  );
-};
-
+        {options.map((option) => (
+          <option key={option.code} value={option.code}>
+            {option.title}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
 export default Select;
