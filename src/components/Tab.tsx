32c7cdb752f9b8bedd9787897174e761cbf16f47
SherbakovSAF <sherbakovsaf@gmail.com>
2025-05-23 22:57:19 +0300

    feat: исправил props для tab

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,44 +1,41 @@
+import React from "react";
 import { type HTMLAttributes } from "react";
 import styles from "./Tab.module.scss";
 import clsx from "clsx";
+import type { OptionItem_I } from "../types/SettingsTypes";
 
 interface TabProps extends HTMLAttributes<HTMLDivElement> {
   view?: "primary" | "secondary";
-  tabs: { code: string; title: string }[];
-  selectedTab?: string | null;
-  onSelectTab: (tab: { code: string; title: string }) => void;
+  tabs: OptionItem_I[];
+  selectedTab?: OptionItem_I | null;
+  onSelectTab: (tab: OptionItem_I) => void;
 }
 
-const Tab: React.FC<TabProps> = ({
-  view = "primary",
-  tabs,
-  selectedTab = null,
-  onSelectTab,
-  ...props
-}) => {
-  console.log(styles.tab_item);
-  return (
-    <div
-      className={clsx(
-        styles.tab,
-        view === "secondary" && styles["tab--secondary"]
-      )}
-      {...props}
-    >
-      {tabs.map((tab, tabIdx) => (
-        <span
-          className={clsx(
-            styles.tab_item,
-            tab.code === selectedTab && styles["tab_item--active"]
-          )}
-          onClick={() => onSelectTab(tab)}
-          key={tabIdx}
-        >
-          {tab.title}
-        </span>
-      ))}
-    </div>
-  );
-};
+const Tab: React.FC<TabProps> = React.memo(
+  ({ view = "primary", tabs, selectedTab = null, onSelectTab, ...props }) => {
+    return (
+      <div
+        className={clsx(
+          styles.tab,
+          view === "secondary" && styles["tab--secondary"]
+        )}
+        {...props}
+      >
+        {tabs.map((tab, tabIdx) => (
+          <span
+            className={clsx(
+              styles.tab_item,
+              tab.code === selectedTab?.code && styles["tab_item--active"]
+            )}
+            onClick={() => onSelectTab(tab)}
+            key={tabIdx}
+          >
+            {tab.title}
+          </span>
+        ))}
+      </div>
+    );
+  }
+);
 
 export default Tab;
