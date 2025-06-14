ef200610d5e562321b9c446019819d70c5bde03f
SherbakovSAF <sherbakovsaf@gmail.com>
2025-06-14 14:11:28 +0300

    feat: add type in components

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,15 +1,32 @@
-import type { HTMLAttributes, PropsWithChildren } from "react";
+import { useMemo, type HTMLAttributes, type PropsWithChildren } from "react";
 import styles from "./Label.module.scss";
 import clsx from "clsx";
+import type { TypeViewComponent } from "../types/UiTypes";
 interface LabelProps
   extends PropsWithChildren<HTMLAttributes<HTMLDListElement>> {
   label: string;
   id: string;
+  view?: TypeViewComponent;
 }
 
-const Label: React.FC<LabelProps> = ({ children, id, label, ...props }) => {
+const Label: React.FC<LabelProps> = ({
+  children,
+  id,
+  label,
+  view = "primary",
+  ...props
+}) => {
+  const getClassByView = useMemo(() => {
+    switch (view) {
+      case "secondary":
+        return styles["label--secondary"];
+
+      default:
+        return "";
+    }
+  }, [view]);
   return (
-    <dl className={clsx(styles.label)} {...props}>
+    <dl className={clsx(styles.label, getClassByView)} {...props}>
       <dt>
         <label htmlFor={id}>{label}</label>
       </dt>
